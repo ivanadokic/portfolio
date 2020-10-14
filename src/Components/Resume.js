a974a70f425198b9be70cf2fc93d9b2d71a86f79
Ivana <ivana_pile@yahoo.com>
2020-10-13 21:39:56 -0400

    added tech images

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -17,13 +17,25 @@ class Resume extends Component {
         </div>
       })
       var skills = this.props.data.skills.map(function (skills) {
-        var className = 'bar-expand ' + skills.name.toLowerCase();
-        return <li key={skills.name}>
-          <span style={{ width: skills.level }} className={className}></span><em>{skills.name}</em></li>
-
-
+        var projectImage = 'images/tech/' + skills.image;
+        return <div key={skills.name} className="columns feature-item">
+          <img className='skill' alt={skills.name} src={projectImage} />
+          <h5>{skills.name}</h5>
+          <p>{skills.description}</p>
+        </div>
       })
+
+
+
+
     }
+
+    //     <li key={skills.name}>
+    //       <span style={{ width: skills.level }} className={className}></span><em>{skills.name}</em></li>
+
+
+    //   })
+    // }
 
     return (
       <section id="resume">
