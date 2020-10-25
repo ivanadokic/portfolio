7f3c63cc5fcea69250c08de8ed2c10293376e06d
Ivana <ivana_pile@yahoo.com>
2020-10-24 21:42:30 -0400

    updates

---
+++ build/precache-manifest.d0a25ab19f11d5a0fd316ea7243159c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bebeb374f360bbff189589612cf045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c64f0b83328ae97cc36",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1da69d766a630181d11e",
+    "url": "/static/js/2.2fcec213.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.2fcec213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c64f0b83328ae97cc36",
+    "url": "/static/js/main.6abd3384.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
